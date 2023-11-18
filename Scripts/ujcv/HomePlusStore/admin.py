56a684f29e1e67b386af24aad90d295a687ac0e5
merarissanchez <150539651+merarissanchez@users.noreply.github.com>
2023-11-18 13:58:28 -0600

    Creación de archivos y de información
    1- Crear formularios y validaciones.
    2- Crear templates, archivos estáticos y el CRUD de Usuario y Artículo.
    3- Crear archivos de admin para verse en el administrador de Django.

--- Scripts/ujcv/HomePlusStore/admin.py
+++ Scripts/ujcv/HomePlusStore/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from HomePlusStore.models import (Usuario, Articulo)
 
+admin.site.register(Usuario)
+admin.site.register(Articulo)
 # Register your models here.
---
+++ Scripts/ujcv/HomePlusStore/forms.py
@@ -0,0 +1,98 @@
+from django import forms
+from .models import Usuario, Articulo
+from django.core.exceptions import ValidationError
+from datetime import date
+from django.utils import timezone
+
+
+
+class UsuarioForm(forms.ModelForm):
+    fecha_nacimiento = forms.DateField(
+        widget=forms.DateInput(
+            format='%Y-%m-%d',
+            attrs={'type': 'date', 'value': '2000-01-01'}
+        )
+    )
+
+    class Meta:
+        model = Usuario
+        fields = "_all_"
+        widgets = {
+            'contraseña': forms.PasswordInput(render_value=True),
+        }
+    
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        if any(char.isdigit() for char in nombre):
+            raise forms.ValidationError("El nombre no puede contener números.")
+        return nombre
+
+    def clean_apellido(self):
+        apellido = self.cleaned_data.get('apellido')
+        if any(char.isdigit() for char in apellido):
+            raise forms.ValidationError("El apellido no puede contener números.")
+        return apellido
+
+    def clean_numero_telefono(self):
+        numero_telefono = self.cleaned_data.get('numero_telefono')
+        if any(char.isalpha() for char in numero_telefono):
+            raise forms.ValidationError("El número de teléfono no puede contener letras.")
+        return numero_telefono
+
+    def clean_all_fields(self):
+        for field_name, field_value in self.cleaned_data.items():
+            if isinstance(field_value, str) and len(field_value) < 10:
+                raise forms.ValidationError(f"El campo {self.fields[field_name].label} debe contener al menos 10 caracteres.")
+        return self.cleaned_data
+    
+    def clean_documento_identidad(self):
+        documento_identidad = self.cleaned_data.get('documento_identidad')
+        if not documento_identidad:
+            raise forms.ValidationError("El documento de identidad es obligatorio. Por favor, sube un documento.")
+        return documento_identidad
+    
+    def clean_fecha_nacimiento(self):
+        fecha_nacimiento = self.cleaned_data.get('fecha_nacimiento')
+        fecha_minima = date(1920, 1, 1)
+        fecha_actual = date.today()
+        edad_minima = 18
+
+        if fecha_nacimiento < fecha_minima or fecha_nacimiento > fecha_actual:
+            raise forms.ValidationError(
+                'La fecha de nacimiento debe estar entre el 1 de enero de 1920 y la fecha actual.'
+            )
+
+        edad = fecha_actual.year - fecha_nacimiento.year - ((fecha_actual.month, fecha_actual.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
+
+        if edad < edad_minima:
+            raise forms.ValidationError(
+                f'Debes ser mayor de {edad_minima} años para registrarte.'
+            )
+
+        return fecha_nacimiento
+
+
+class ArticuloForm(forms.ModelForm):
+    class Meta:
+        model = Articulo
+        fields = "_all_"
+
+    def clean_imagen(self):
+        imagen = self.cleaned_data.get('imagen')
+        if not imagen:
+            raise forms.ValidationError("La imagen del artículo es obligatoria. Por favor, sube una imagen.")
+        return imagen
+
+    def clean_precio(self):
+        precio = self.cleaned_data.get('precio')
+        if precio is not None and precio < 500:
+            raise forms.ValidationError("El precio del artículo no puede ser menor de 500.")
+        return precio
+
+    def save(self, commit=True):
+        articulo = super().save(commit)
+        
+        # Registra el precio en el historial
+        PrecioHistorico.objects.create(articulo=articulo, precio=articulo.precio, fecha=timezone.now())
+        
+        return articulo
