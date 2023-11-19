65557f283ba90e77447dd726cfd754fe87173c57
Marvin Flores <150742157+FloresGamer@users.noreply.github.com>
2023-11-18 18:37:57 -0600

    view, url, form, crud
    Cree las vistas, defini las urls, cree la parte delos formulario, cree validaciones para sucursal y colaborador lo agregue al administrador de django y cree el crud con los html

--- Scripts/ujcv/HomePlusStore/admin.py
+++ Scripts/ujcv/HomePlusStore/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from HomePlusStore.models import (Usuario, Articulo, CategoriaArticulo, Departamento,Cargo)
+from HomePlusStore.models import (Usuario, Articulo, CategoriaArticulo, Departamento,Cargo, Colaborador, Sucursal, PrecioHistorico)
 
 admin.site.register(Usuario)
 admin.site.register(Articulo)
 admin.site.register(CategoriaArticulo)
 admin.site.register(Departamento)
 admin.site.register(Cargo)
+admin.site.register(Colaborador)
+admin.site.register(Sucursal)
+admin.site.register(PrecioHistorico)
 # Register your models here.
